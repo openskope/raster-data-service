9362664be6c4f185078bd8ea841e456d2e10d21d
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-08-23 12:18:23 -0700

    remove flask dependency and don't install gdal-bin

--- python/skope_analysis/setup.py
+++ python/skope_analysis/setup.py
@@ -12,7 +12,5 @@ setup(
     packages=['skope_analysis'],
     package_dir={'': 'src'},
     data_files = [("", ["LICENSE.txt"])],
-    install_requires=[
-        'Flask >= 1.0.2'
-    ]
+    install_requires=[]
 )
