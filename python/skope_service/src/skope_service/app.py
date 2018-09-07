47af581af7eab8e29d0e7ef1dcc77140ab6fe004
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-09-06 22:38:48 -0700

    add hardwired endpoint for timeseries GET endpoint with passing tests

--- python/skope_service/src/skope_service/app.py
+++ python/skope_service/src/skope_service/app.py
@@ -12,6 +12,22 @@ else:
 def get_status():
     return jsonify({'name': 'SKOPE Timeseries Service'})
 
+@app.route(APPLICATION_ROOT + '/timeseries/<dataset_id>/<variable_name>')
+def get_timeseries(dataset_id, variable_name):
+    response_body = {
+        'datasetId': dataset_id,
+        'variableName': variable_name,
+        'boundaryGeometry': { 
+            'type': 'Point',
+            'coordinates': [-123,45]
+        },
+        'start': '0',
+        'end': '4',
+        'values': [100,200,300,400,500]
+    }
+    return jsonify(response_body)
+
+
 if __name__ == '__main__':
     app.run(port=8001, debug=True)
 
