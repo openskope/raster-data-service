08770297d1953df5530ae68b7afbb191d207ca36
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-09-11 14:03:27 -0700

    address pyline warnings in tests

--- python/skope/tests/gdal_wrappers/test_create_dataset.py
+++ python/skope/tests/gdal_wrappers/test_create_dataset.py
@@ -1,45 +1,25 @@
-import affine
-import numpy as np
+'''Tests of the skope.create_dataset function.'''
 import os
-import pytest
-import skope
-
-from osgeo import gdal
 from typing import List, Dict
 
-################################################################################
-# Module-scoped constants defining properties of the test dataset.
-################################################################################
+import affine
+import numpy as np
+import pytest
+from osgeo import gdal
 
-DATASET_ROW_COUNT        = 4
-DATASET_COLUMN_COUNT     = 5
-DATASET_BAND_COUNT       = 6
-DATASET_ORIGIN_LONGITUDE = -123
-DATASET_ORIGIN_LATITUDE  = 45
-DATASET_PIXEL_WIDTH      = 1.0
-DATASET_PIXEL_HEIGHT     = 2.0
+import skope
 
-################################################################################
-# Test fixtures run once for this module.
-################################################################################
+# pylint: disable=redefined-outer-name
 
 @pytest.fixture(scope='module')
 def path_to_dataset(test_dataset_filename) -> str:
     '''Create a new dataset file and return its path.'''
     path_to_dataset = test_dataset_filename(__file__)
-    skope.create_dataset(
-        filename     = path_to_dataset,
-        file_format  = 'GTiff',
-        pixel_type   = gdal.GDT_Float32, 
-        rows         = DATASET_ROW_COUNT, 
-        cols         = DATASET_COLUMN_COUNT, 
-        bands        = DATASET_BAND_COUNT,
-        origin_long  = DATASET_ORIGIN_LONGITUDE,
-        origin_lat   = DATASET_ORIGIN_LATITUDE,
-        pixel_width  = DATASET_PIXEL_WIDTH,
-        pixel_height = DATASET_PIXEL_HEIGHT,
-        coordinate_system='WGS84'
-    )
+    skope.create_dataset(filename=path_to_dataset, file_format='GTiff',
+                         pixel_type=gdal.GDT_Float32, rows=4, cols=5,
+                         bands=6, origin_long=-123, origin_lat=45,
+                         pixel_width=1.0, pixel_height=2.0,
+                         coordinate_system='WGS84')
     return path_to_dataset
 
 @pytest.fixture(scope='module')
@@ -74,39 +54,37 @@ def first_band(dataset) -> gdal.Band:
     '''Return band 1 of the new dataset.'''
     return dataset.GetRasterBand(1)
 
-################################################################################
-# Tests of the results of using the create_dataset_file() function.
-################################################################################
+# pylint: disable=redefined-outer-name, missing-docstring, line-too-long
 
 def test_created_datafile_exists(path_to_dataset: str):
-    assert os.path.isfile(path_to_dataset) 
+    assert os.path.isfile(path_to_dataset)
 
 def test_dataset_object_is_gdal_dataset(dataset: gdal.Dataset):
     assert str((type(dataset))) == "<class 'osgeo.gdal.Dataset'>"
 
 def test_dataset_format_is_geotiff(dataset: gdal.Dataset):
-     assert dataset.GetDriver().LongName == "GeoTIFF"
+    assert dataset.GetDriver().LongName == "GeoTIFF"
 
 def test_pixel_type_is_float32(first_band: gdal.Band):
-     assert gdal.GetDataTypeName(first_band.DataType) == 'Float32'
+    assert gdal.GetDataTypeName(first_band.DataType) == 'Float32'
 
 def test_dataset_height_in_pixels_is_4(dataset: gdal.Dataset):
-    assert dataset.RasterYSize == DATASET_ROW_COUNT
+    assert dataset.RasterYSize == 4
 
 def test_dataset_width_in_pixels_is_5(dataset: gdal.Dataset):
-    assert dataset.RasterXSize == DATASET_COLUMN_COUNT
+    assert dataset.RasterXSize == 5
 
 def test_dataset_band_count_is_6(dataset: gdal.Dataset):
-    assert dataset.RasterCount == DATASET_BAND_COUNT
+    assert dataset.RasterCount == 6
 
 def test_pixel_width_is_1(geotransform: List[float]):
-    assert geotransform[1] == DATASET_PIXEL_WIDTH
+    assert geotransform[1] == 1.0
 
 def test_pixel_height_is_2(geotransform: List[float]):
-    assert geotransform[5] == -DATASET_PIXEL_HEIGHT
+    assert geotransform[5] == -2.0
 
 def test_geotransform_is_north_up(geotransform: List[float]):
-    assert (geotransform[2],geotransform[4]) == (0,0)
+    assert (geotransform[2], geotransform[4]) == (0, 0)
 
 def test_projection_is_wgs84(dataset: gdal.Dataset):
     assert dataset.GetProjection()[8:14] == 'WGS 84'
@@ -126,11 +104,11 @@ def test_projected_coordinates_of_pixel_4_3_is_southeast_corner(affine_matrix: L
 def test_inverse_projection_of_southeast_corner_is_pixel_5_4(inverse_affine: List[float]):
     assert (inverse_affine * (-118.0, 37.0)) == (5, 4)
 
-@pytest.mark.parametrize("band_index", range(0,DATASET_BAND_COUNT))
+@pytest.mark.parametrize("band_index", range(0, 6))
 def test_initial_pixel_values_all_zero_in_band(dataset: gdal.Dataset, band_index: int):
     band_number = band_index + 1
     band_pixels = dataset.GetRasterBand(band_number).ReadAsArray()
-    assert np.array_equal(band_pixels, np.array([[0.,0.,0.,0.,0.],
-                                                 [0.,0.,0.,0.,0.],
-                                                 [0.,0.,0.,0.,0.],
-                                                 [0.,0.,0.,0.,0.]] ))
+    assert np.array_equal(band_pixels, np.array([[0., 0., 0., 0., 0.],
+                                                 [0., 0., 0., 0., 0.],
+                                                 [0., 0., 0., 0., 0.],
+                                                 [0., 0., 0., 0., 0.]]))
--- python/skope/tests/gdal_wrappers/test_read_write_dataset_values.py
+++ python/skope/tests/gdal_wrappers/test_read_write_dataset_values.py
@@ -1,95 +1,73 @@
+'''Tests of the write_pixel, read_pixel, write_band, and read_band functions.'''
 import numpy as np
 import pytest
-import skope
-
 from osgeo import gdal
 
-################################################################################
-# Module-scoped constants defining properties of the test dataset.
-################################################################################
+import skope
 
-DATASET_ROW_COUNT            = 2
-DATASET_COLUMN_COUNT         = 2
-DATASET_BAND_COUNT           = 2
-DATASET_ORIGIN_LONGITUDE     = -123
-DATASET_ORIGIN_LATITUDE      = 45
-DATASET_PIXEL_WIDTH          = 1.0
-DATASET_PIXEL_HEIGHT         = 1.0
-DATASET_NODATA_VALUE         = float('nan')
-
-################################################################################
-# Test fixtures run once for this module.
-################################################################################
+# pylint: disable=redefined-outer-name
 
 @pytest.fixture(scope='module')
 def array_assigned_to_band_index_0():
-    return np.array([[1,2],[3,4]])
+    '''Return pixel values to be assigned to band index 0.'''
+    return np.array([[1, 2], [3, 4]])
 
 @pytest.fixture(scope='module')
 def array_assigned_to_band_index_1():
-    return np.array([[11,12],[13,14]])
+    '''Return pixel values to be assigned to band index 1.'''
+    return np.array([[11, 12], [13, 14]])
 
 @pytest.fixture(scope='module')
-def dataset(test_dataset_filename, 
-            array_assigned_to_band_index_0, 
+def dataset(test_dataset_filename,
+            array_assigned_to_band_index_0,
             array_assigned_to_band_index_1) -> gdal.Dataset:
     '''Create a new dataset, and set its values using write_band() and
     write_pixel() functions.'''
 
     # create the new dataset
-    dataset = skope.create_dataset(
-        filename     = test_dataset_filename(__file__),
-        file_format  = 'GTiff',
-        pixel_type   = gdal.GDT_Float32, 
-        rows         = DATASET_ROW_COUNT, 
-        cols         = DATASET_COLUMN_COUNT, 
-        bands        = DATASET_BAND_COUNT,
-        origin_long  = DATASET_ORIGIN_LONGITUDE,
-        origin_lat   = DATASET_ORIGIN_LATITUDE,
-        pixel_width  = DATASET_PIXEL_WIDTH,
-        pixel_height = DATASET_PIXEL_HEIGHT,
-        coordinate_system='WGS84'
-    )
+    dataset = skope.create_dataset(filename=test_dataset_filename(__file__),
+                                   file_format='GTiff', pixel_type=gdal.GDT_Float32,
+                                   rows=2, cols=2, bands=2, origin_long=-123,
+                                   origin_lat=45, pixel_width=1.0, pixel_height=1.0,
+                                   coordinate_system='WGS84')
 
     # set the values in band 1 with a call to write_band
-    skope.write_band(dataset, 0, array_assigned_to_band_index_0, DATASET_NODATA_VALUE)
+    skope.write_band(dataset, 0, array_assigned_to_band_index_0, float('Nan'))
 
     # set the values in band 2 with calls to write_pixel
-    skope.write_pixel(dataset, 1, 0, 0, array_assigned_to_band_index_1[0,0])
-    skope.write_pixel(dataset, 1, 0, 1, array_assigned_to_band_index_1[0,1])
-    skope.write_pixel(dataset, 1, 1, 0, array_assigned_to_band_index_1[1,0])
-    skope.write_pixel(dataset, 1, 1, 1, array_assigned_to_band_index_1[1,1])
+    skope.write_pixel(dataset, 1, 0, 0, array_assigned_to_band_index_1[0, 0])
+    skope.write_pixel(dataset, 1, 0, 1, array_assigned_to_band_index_1[0, 1])
+    skope.write_pixel(dataset, 1, 1, 0, array_assigned_to_band_index_1[1, 0])
+    skope.write_pixel(dataset, 1, 1, 1, array_assigned_to_band_index_1[1, 1])
 
     return dataset
 
-################################################################################
-# Tests of dataset read and write functions.
-# ################################################################################
+# pylint: disable=redefined-outer-name, missing-docstring, line-too-long
 
 def test_write_band_sets_assigns_expected_pixel_values(
         dataset: gdal.Dataset, array_assigned_to_band_index_0):
     assert np.array_equal(
-        array_assigned_to_band_index_0, 
+        array_assigned_to_band_index_0,
         dataset.GetRasterBand(1).ReadAsArray()
     )
-    
+
 def test_write_pixel_sets_assigns_expected_pixel_values(
         dataset: gdal.Dataset, array_assigned_to_band_index_1):
     assert np.array_equal(
-        array_assigned_to_band_index_1, 
+        array_assigned_to_band_index_1,
         dataset.GetRasterBand(2).ReadAsArray()
     )
 
 def test_read_band_returns_expected_pixel_values(
         dataset: gdal.Dataset, array_assigned_to_band_index_0):
     assert np.array_equal(
-        array_assigned_to_band_index_0, 
+        array_assigned_to_band_index_0,
         skope.read_band(dataset, 0)
     )
 
 def test_read_pixel_returns_expected_pixel_values(
         dataset: gdal.Dataset, array_assigned_to_band_index_1):
-    assert skope.read_pixel(dataset, 1, 0, 0) == array_assigned_to_band_index_1[0,0]
-    assert skope.read_pixel(dataset, 1, 0, 1) == array_assigned_to_band_index_1[0,1]
-    assert skope.read_pixel(dataset, 1, 1, 0) == array_assigned_to_band_index_1[1,0]
-    assert skope.read_pixel(dataset, 1, 1, 1) == array_assigned_to_band_index_1[1,1] 
+    assert skope.read_pixel(dataset, 1, 0, 0) == array_assigned_to_band_index_1[0, 0]
+    assert skope.read_pixel(dataset, 1, 0, 1) == array_assigned_to_band_index_1[0, 1]
+    assert skope.read_pixel(dataset, 1, 1, 0) == array_assigned_to_band_index_1[1, 0]
+    assert skope.read_pixel(dataset, 1, 1, 1) == array_assigned_to_band_index_1[1, 1]
