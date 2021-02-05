88b1af309393051fe582a617e2946118bb781ab9
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-02-04 20:42:29 -0800

    Moved alternative ptyon implmentation to new timeseries-python repo.

--- python/skope/src/skope/__init__.py
+++
@@ -1,4 +0,0 @@
-'''Functions and classes wrapping GDAL.'''
-
-# pylint: disable=wildcard-import
-from skope.raster_dataset import *
--- python/skope_service/src/skope_service/__init__.py
+++
@@ -1,5 +0,0 @@
-'''SKOPE services'''
-
-# pylint: disable=wildcard-import
-
-from skope_service.flask_app import app
